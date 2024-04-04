9983e44f02ab4db5cbbd940e1c1e7347465627a7
Harounas <140737983+Harounas@users.noreply.github.com>
2024-04-04 17:47:48 +0100

    Update myapp2.py

--- myapp2.py
+++ myapp2.py
@@ -253,55 +253,6 @@ st.write(dff)
 dfff=pd.merge(dff,dff.groupby(['siteregion_crf']).sum().reset_index(),on="siteregion_crf")
 st.write(dff)
 
-
-# Initializing the chart
-chart = Chart(width="640px", height="360px", display="manual")
-
-
-data = Data()
-data.add_df(df)
-
-chart = Chart(width="640px", height="360px")
-
-chart.animate(data)
-
-obj = create_vizzu_obj(dff)
-
-# Preset plot usage. Preset plots works directly with DataFrames.
-bar_obj = bar_chart(df,
-            x = "hiv_rdt", 
-            y = "Count",
-            title= "1.Using preset plot function `bar_chart()`"
-            )
-
-# Animate with defined arguments 
-anim_obj = beta_vizzu_animate( bar_obj,
-    x = "siteregion_crf",
-    y =  ["Count", "hiv_rdt"],
-    title = "Animate with beta_vizzu_animate () function",
-    label= "Count",
-    color="",
-    legend="color",
-    sort="byValue",
-    reverse=True,
-    align="center",
-    split=False,
-)
-
-# Animate with general dict based arguments 
-_dict = {"size": {"set": "Popularity"}, 
-    "geometry": "circle",
-    "coordSystem": "polar",
-    "title": "Animate with vizzu_animate () function",
-    }
-anim_obj2 = vizzu_animate(anim_obj,_dict)
-
-# Visualize within Streamlit
-with st.container(): # Maintaining the aspect ratio
-    st.button("Animate")
-    vizzu_plot(anim_obj2)
-    
-
 select_catcol=st.multiselect('Please select categorical column to make  a bar plot:',df.select_dtypes(include='object').columns)
 
 if select_catcol:
