f3d545581a271b6cb16a015da07d87b233a448e0
Harounas <140737983+Harounas@users.noreply.github.com>
2024-04-26 12:26:08 +0100

    Update myapp2.py

--- myapp2.py
+++ myapp2.py
@@ -88,7 +88,17 @@ def main():
   
 df=main() 
 
-       
+def are_rows_same(row1, row2):
+    if len(row1) != len(row2):
+        return False
+    
+    for i in range(len(row1)):
+        if row1[i] != row2[i]:
+            return False
+    
+    return True
+
+st.write(are_rows_same(df['participantid_crf'], df['participantid_rdt']))       
 #df = conn.read(spreadsheet=url)
 df['date_crf'] = pd.to_datetime(df['date_crf'], errors='coerce', format='%Y-%m-%d')
 df=df.dropna(subset=['siteregion_crf'])
