8cc590c32a65835e2cce54de93188a1ec5597db6
Harouna <140737983+Harounas@users.noreply.github.com>
2024-07-04 14:02:33 +0100

    Update myapp2.py

--- myapp2.py
+++ myapp2.py
@@ -21,7 +21,7 @@ from scipy.stats import chi2_contingency, fisher_exact
 from scipy.stats import zscore
 
 #url="https://docs.google.com/spreadsheets/d/1lyBADWC8fAhUNw4LOcIoOSYBqNeEbVs_KU71O8rKqfs/edit?usp=sharing"
-url="https://docs.google.com/spreadsheets/d/1snI1XZVsUvCgwG6yXj_tzSxPZ19N5DJ-mcV-F1ojQb0/edit?usp=sharing"
+url="https://docs.google.com/spreadsheets/d/18b5Ae4aIUgTJrdlVKrvo7D-V6DTy1fAvHkIdHMjsUY4/edit?usp=sharing"
 conn = st.connection("gsheets", type=GSheetsConnection)
 st.title("Data summary sentinel project")
 st.markdown("---")
