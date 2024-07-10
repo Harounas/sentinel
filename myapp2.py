930c0a86b203895c032d1912b1211244bb15e968
Harouna <140737983+Harounas@users.noreply.github.com>
2024-07-10 14:22:40 +0100

    Update myapp2.py

--- myapp2.py
+++ myapp2.py
@@ -81,8 +81,8 @@ def main():
 
         df = pd.read_csv(StringIO(r.text),  low_memory=False)
        # st.write(df.head())
-    else:
-        df=df0     
+    #else:
+        #df=df0     
 
     return df
   
