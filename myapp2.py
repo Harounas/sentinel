39070962179857b31313d38137f7f95a92a38d0e
Harouna <140737983+Harounas@users.noreply.github.com>
2024-07-25 13:17:54 +0100

    Update myapp2.py

--- myapp2.py
+++ myapp2.py
@@ -316,10 +316,8 @@ monthly_ticks = pd.date_range(start=dff['date_crf'].iloc[0], end=dff['date_crf']
 tick_values = [mdates.date2num(date) for date in monthly_ticks]
 # Generate labels for ticks
 labels = [date.strftime('%Y-%m-%d') for date in monthly_ticks]
-
+plt.xticks(ticks=ick_values, labels=labels, rotation=90)
 # Set ticks and labels
-plt.gca().set_xticks(tick_values)
-plt.gca().set_xticklabels(labels, rotation=90)
 
 ax.tick_params(axis='x', labelsize=15)
 ax.set_xlabel('Date')
