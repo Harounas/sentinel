35686651544686515d2c04b3c2ace4d933b62766
Harouna <140737983+Harounas@users.noreply.github.com>
2024-07-29 09:33:18 +0100

    Update myapp2.py

--- myapp2.py
+++ myapp2.py
@@ -605,7 +605,7 @@ df = df.dropna(axis=1, how='all')
 #independent_variables = st.multiselect("Choose independent variables", options=[col for col in df.columns if col != target_variable],default=default_independent)
 # Default selection
 default_target = df.columns[5]  # First column as default target variable
-default_independent = [col for col in df.columns if col != default_target]  # All other columns as default independent variables
+default_independent = [col for col in df.columns if col != default_target][:10]  # All other columns as default independent variables
 
 # Choose dependent variable
 target_variable = st.selectbox("Choose the dependent variable", options=df.columns, index=df.columns.get_loc(default_target))
