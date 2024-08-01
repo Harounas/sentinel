3cc261f04381df3e7335b450f1da3d3c1ed7953f
Harouna <140737983+Harounas@users.noreply.github.com>
2024-08-01 11:33:00 +0100

    Update myapp2.py

--- myapp2.py
+++ myapp2.py
@@ -544,7 +544,7 @@ if select_catcol and select_numcol:
    st.write("Selected categorical column is : ", select_catcol[0], "" " and Selected numerical column is : ",select_numcol[0] )
    fig, ax = plt.subplots(figsize=(8, 6))
    df1.boxplot(by=select_catcol[0], column=select_numcol[0], ax=ax,widths=0.5)
-   ax.set_title(' ', fontsize=12) 
+   ax.set_title(' ') 
    ax.set_xlabel(f'{select_catcol[0]}', fontsize=12)  # Adjust fontsize as needed
    ax.set_ylabel(f'{select_numcol[0]}', fontsize=12) 
    ax.tick_params(axis='x', labelsize=12)  # Reduce x-axis tick label size
